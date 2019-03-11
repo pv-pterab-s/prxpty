8ccd8bf060a413fd3019e10826f1a32600ba6a37
Gallagher Donovan Pryor <gallagher.pryor@gmail.com>
2019-03-11 14:40:41 +0000

    log.ls logging utilities; tested
    corrected chk to properly invoke arbitrarily nested tests

--- test/00_test.sh
+++ test/00_test.sh
@@ -15,24 +15,24 @@ function exec_test_to_regexp {
 
   INPUT="pair0/env"
   echo $(test_to_regexp $INPUT)
-  find test -regex "$(test_to_regexp $INPUT)"
+  find test -regex test"$(test_to_regexp $INPUT)"
 
   INPUT="pair0"
   echo $(test_to_regexp $INPUT)
-  find test -regex "$(test_to_regexp $INPUT)"
+  find test -regex test"$(test_to_regexp $INPUT)"
 
   INPUT="pair0/next1/env"
   echo $(test_to_regexp $INPUT)
-  find test -regex "$(test_to_regexp $INPUT)"
+  find test -regex test"$(test_to_regexp $INPUT)"
 }
 
 diff <(exec_test_to_regexp) - <<EOF
 pair0 %% env
 %% pair0
 pair0 next1 %% env
-^test/[0-9]+_pair0/[0-9]+_env\(\.[^.]*\|$\)
-test/02_pair0/00_env.sh
-^test/[0-9]+_pair0\(\.[^.]*\|$\)
-test/02_pair0
-^test/[0-9]+_pair0/[0-9]+_next1/[0-9]+_env\(\.[^.]*\|$\)
+/[0-9]+_pair0/[0-9]+_env\(\.[^.]*\|$\)
+test/05_pair0/00_env.sh
+/[0-9]+_pair0\(\.[^.]*\|$\)
+test/05_pair0
+/[0-9]+_pair0/[0-9]+_next1/[0-9]+_env\(\.[^.]*\|$\)
 EOF
