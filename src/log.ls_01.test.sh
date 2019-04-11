87f16080f61a2baa665f4309774291dae7fb9865
Gallagher Donovan Pryor <gallagher.pryor@gmail.com>
2019-04-11 12:05:41 -0400

    fix: log, debug tests randomly fail due to stream buffering
    stream buffering is out of our control. drop attempts at checking
    one-line-per-stream-message policy from log, debug tests
    
    hopefully the functionality will be guaranteed in the mix of upper level
    component testing

--- src/log.ls_01.test.sh
+++ src/log.ls_01.test.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 set -e
 
-# \TEST00 w/ filename as option
-LOGTMP=$(mktemp)
-OUTTMP=$(mktemp)
-echo log to $LOGTMP while out to $OUTTMP
-for I in {0..2} ; do
-  echo $I
-  sleep 1s
-done | src/log.ls $LOGTMP | tee $OUTTMP
+# \TEST00 log <path> emits log to path and stream to stdout
+LOG=$(mktemp)
+STDOUT=$(mktemp)
 
-diff $OUTTMP - <<EOF
-0
-1
-2
+echo hey there | src/log.ls $LOG > $STDOUT
+
+diff $STDOUT - <<EOF
+hey there
 EOF
 
 
 # \TEST01 w/o filename
-OUTTMP=$(mktemp)
-echo log to $OUTTMP
-for I in {0..2} ; do
-  echo $I
-  sleep 1s
-done | src/log.ls | tee $OUTTMP
-
-grep -ne '^............MAo=' $OUTTMP | grep -e '^1:'
-grep -ne '^............MQo=' $OUTTMP | grep -e '^2:'
-grep -ne '^............Mgo=' $OUTTMP | grep -e '^3:'
+echo hey there | src/log.ls > $LOG
+[ $(cat $LOG | wc -l) = 1 ]
